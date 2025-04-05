80b1d3005e035b143c23b460d9f47e97a86f75b8
Miguel Vieira <152816803+MiguelOlivieira@users.noreply.github.com>
2025-04-05 13:42:53 -0300

    First commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone: ");
+Iphone iphone = new Iphone(numero: "123456", modelo: "iphone13", imei: "556789", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia(numero: "1234567", modelo: "NokiaLumia", imei: "55349", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
